d92e214cca3a7938ec4109ce18494875281e838e
Dtwosam <42391449+Dtwosam@users.noreply.github.com>
2026-08-23 21:39:12 +0100

    feat: export graduation breakout public API

--- python/src/shreks_brain/setups/__init__.py
+++ python/src/shreks_brain/setups/__init__.py
@@ -1,10 +1,18 @@
 from .fresh_launch import assess_fresh_launch
+from .graduation_breakout import assess_graduation_breakout
 from .models import (
     FRESH_LAUNCH_CONFIRMATIONS_REQUIRED,
     FRESH_LAUNCH_SETUP_NAME,
+    GRADUATION_BREAKOUT_CONFIRMATIONS_REQUIRED,
+    GRADUATION_BREAKOUT_SETUP_NAME,
     FreshLaunchAssessment,
     FreshLaunchPolicy,
     FreshLaunchReasonCode,
+    GraduationBreakoutAssessment,
+    GraduationBreakoutFinding,
+    GraduationBreakoutPolicy,
+    GraduationBreakoutReasonCode,
+    GraduationContext,
     SetupFinding,
     SetupState,
 )
@@ -12,10 +20,18 @@ from .models import (
 __all__ = (
     "FRESH_LAUNCH_CONFIRMATIONS_REQUIRED",
     "FRESH_LAUNCH_SETUP_NAME",
+    "GRADUATION_BREAKOUT_CONFIRMATIONS_REQUIRED",
+    "GRADUATION_BREAKOUT_SETUP_NAME",
     "FreshLaunchAssessment",
     "FreshLaunchPolicy",
     "FreshLaunchReasonCode",
+    "GraduationBreakoutAssessment",
+    "GraduationBreakoutFinding",
+    "GraduationBreakoutPolicy",
+    "GraduationBreakoutReasonCode",
+    "GraduationContext",
     "SetupFinding",
     "SetupState",
     "assess_fresh_launch",
+    "assess_graduation_breakout",
 )
