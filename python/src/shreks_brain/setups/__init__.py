69b13218d6f9252fe85c5d848151346f7fc8783d
Dtwosam <42391449+Dtwosam@users.noreply.github.com>
2026-08-23 22:04:31 +0100

    feat(b5): export first pullback public API

--- python/src/shreks_brain/setups/__init__.py
+++ python/src/shreks_brain/setups/__init__.py
@@ -1,10 +1,17 @@
+from .first_pullback import assess_first_pullback
 from .fresh_launch import assess_fresh_launch
 from .graduation_breakout import assess_graduation_breakout
 from .models import (
+    FIRST_PULLBACK_CONFIRMATIONS_REQUIRED,
+    FIRST_PULLBACK_SETUP_NAME,
     FRESH_LAUNCH_CONFIRMATIONS_REQUIRED,
     FRESH_LAUNCH_SETUP_NAME,
     GRADUATION_BREAKOUT_CONFIRMATIONS_REQUIRED,
     GRADUATION_BREAKOUT_SETUP_NAME,
+    FirstPullbackAssessment,
+    FirstPullbackFinding,
+    FirstPullbackPolicy,
+    FirstPullbackReasonCode,
     FreshLaunchAssessment,
     FreshLaunchPolicy,
     FreshLaunchReasonCode,
@@ -13,15 +20,22 @@ from .models import (
     GraduationBreakoutPolicy,
     GraduationBreakoutReasonCode,
     GraduationContext,
+    PullbackContext,
     SetupFinding,
     SetupState,
 )
 
 __all__ = (
+    "FIRST_PULLBACK_CONFIRMATIONS_REQUIRED",
+    "FIRST_PULLBACK_SETUP_NAME",
     "FRESH_LAUNCH_CONFIRMATIONS_REQUIRED",
     "FRESH_LAUNCH_SETUP_NAME",
     "GRADUATION_BREAKOUT_CONFIRMATIONS_REQUIRED",
     "GRADUATION_BREAKOUT_SETUP_NAME",
+    "FirstPullbackAssessment",
+    "FirstPullbackFinding",
+    "FirstPullbackPolicy",
+    "FirstPullbackReasonCode",
     "FreshLaunchAssessment",
     "FreshLaunchPolicy",
     "FreshLaunchReasonCode",
@@ -30,8 +44,10 @@ __all__ = (
     "GraduationBreakoutPolicy",
     "GraduationBreakoutReasonCode",
     "GraduationContext",
+    "PullbackContext",
     "SetupFinding",
     "SetupState",
+    "assess_first_pullback",
     "assess_fresh_launch",
     "assess_graduation_breakout",
 )
