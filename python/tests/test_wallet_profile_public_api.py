ffbe376751fa84748774d2d52ebc8cb9dd55a1fa
Dtwosam <42391449+Dtwosam@users.noreply.github.com>
2026-08-24 16:11:15 +0100

    test: define D4 wallet independence contract

--- python/tests/test_wallet_profile_public_api.py
+++ python/tests/test_wallet_profile_public_api.py
@@ -1,21 +1,24 @@
 import shreks_brain.wallets as wallets
 
 
-def test_wallet_public_api_is_exact_after_d3() -> None:
-    assert wallets.__all__ == (
-        "WalletActionKind",
-        "WalletObservation",
-        "WalletObservationEvidence",
-        "WalletTradeEpisode",
-        "WalletTradeEpisodeState",
-        "WalletTradeEvidenceQuality",
-        "WalletTradeFinding",
-        "WalletTradeFindingCode",
-        "WalletTradeReconstruction",
-        "reconstruct_wallet_trades",
-        "WalletEpisodeProfileContext",
-        "WalletProfile",
-        "WalletProfilePolicy",
-        "WalletRegimeProfile",
-        "build_wallet_profile",
-    )
+EXPECTED_D3_PREFIX = (
+    "WalletActionKind",
+    "WalletObservation",
+    "WalletObservationEvidence",
+    "WalletTradeEpisode",
+    "WalletTradeEpisodeState",
+    "WalletTradeEvidenceQuality",
+    "WalletTradeFinding",
+    "WalletTradeFindingCode",
+    "WalletTradeReconstruction",
+    "reconstruct_wallet_trades",
+    "WalletEpisodeProfileContext",
+    "WalletProfile",
+    "WalletProfilePolicy",
+    "WalletRegimeProfile",
+    "build_wallet_profile",
+)
+
+
+def test_wallet_public_api_preserves_exact_d3_prefix() -> None:
+    assert wallets.__all__[: len(EXPECTED_D3_PREFIX)] == EXPECTED_D3_PREFIX
