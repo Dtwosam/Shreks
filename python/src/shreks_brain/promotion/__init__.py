367a5911e37e97f919871e409873eab09cd79760
Dtwosam <42391449+Dtwosam@users.noreply.github.com>
2026-08-25 09:55:33 +0100

    feat: expose E8 promotion contracts

---
+++ python/src/shreks_brain/promotion/__init__.py
@@ -0,0 +1,20 @@
+from .models import (
+    PROMOTION_SCHEMA_VERSION,
+    PromotionAssessment,
+    PromotionDecision,
+    PromotionGateCode,
+    PromotionGateResult,
+    PromotionGateStatus,
+    PromotionPolicy,
+)
+
+
+__all__ = (
+    "PROMOTION_SCHEMA_VERSION",
+    "PromotionDecision",
+    "PromotionGateStatus",
+    "PromotionGateCode",
+    "PromotionPolicy",
+    "PromotionGateResult",
+    "PromotionAssessment",
+)
