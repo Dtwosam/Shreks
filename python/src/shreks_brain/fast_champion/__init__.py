3a7874fcfc6ecd2641d86c43b9226b202b941bb6
Dtwosam <42391449+Dtwosam@users.noreply.github.com>
2026-09-04 17:30:04 +0100

    fix: preserve sealed FL8.5 public API

--- python/src/shreks_brain/fast_champion/__init__.py
+++ python/src/shreks_brain/fast_champion/__init__.py
@@ -1,8 +1,3 @@
-from .first_builder import (
-    FAST_FIRST_CHAMPION_BUILDER_VERSION,
-    FastFirstChampionBuildResult,
-    build_fast_first_champion,
-)
 from .builder import build_fast_forecast_champion
 from .codec import read_fast_forecast_champion, write_fast_forecast_champion
 from .models import (
@@ -14,9 +9,6 @@ from .models import (
 )
 
 __all__ = (
-    "FAST_FIRST_CHAMPION_BUILDER_VERSION",
-    "FastFirstChampionBuildResult",
-    "build_fast_first_champion",
     "FAST_FORECAST_CHAMPION_SCHEMA_NAME",
     "FAST_FORECAST_CHAMPION_SCHEMA_VERSION",
     "FastForecastChampionSelection",
