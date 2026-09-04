3d294282d7df5feb1280c87857128a86d7ba4599
Dtwosam <42391449+Dtwosam@users.noreply.github.com>
2026-09-04 10:58:19 +0100

    feat: export deterministic candidate matrix API

--- python/src/shreks_brain/fast_deterministic_campaign/__init__.py
+++ python/src/shreks_brain/fast_deterministic_campaign/__init__.py
@@ -1,5 +1,11 @@
 from .engine import run_fast_deterministic_chronological_campaign
 from .models import FastDeterministicCampaignRow
+from .matrix import (
+    FAST_DETERMINISTIC_CANDIDATE_MATRIX_VERSION,
+    FastDeterministicCandidateCampaignSpec,
+    FastDeterministicCandidateMatrixResult,
+    run_fast_deterministic_candidate_matrix,
+)
 from .paper_evidence import (
     FastDeterministicCampaignPaperEvidence,
     materialize_fast_deterministic_campaign_paper_evidence,
@@ -7,8 +13,12 @@ from .paper_evidence import (
 
 
 __all__ = (
+    "FAST_DETERMINISTIC_CANDIDATE_MATRIX_VERSION",
+    "FastDeterministicCandidateCampaignSpec",
+    "FastDeterministicCandidateMatrixResult",
     "FastDeterministicCampaignPaperEvidence",
     "FastDeterministicCampaignRow",
     "materialize_fast_deterministic_campaign_paper_evidence",
+    "run_fast_deterministic_candidate_matrix",
     "run_fast_deterministic_chronological_campaign",
 )
