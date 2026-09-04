c03a1f056d237f160e4592f67073d0f5ef0edf19
Dtwosam <42391449+Dtwosam@users.noreply.github.com>
2026-09-04 10:29:23 +0100

    feat: export deterministic chronological campaign API

---
+++ python/src/shreks_brain/fast_deterministic_campaign/__init__.py
@@ -0,0 +1,8 @@
+from .engine import run_fast_deterministic_chronological_campaign
+from .models import FastDeterministicCampaignRow
+
+
+__all__ = (
+    "FastDeterministicCampaignRow",
+    "run_fast_deterministic_chronological_campaign",
+)
