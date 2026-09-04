8bd2767f0fa9385d50b52cf226606331a7c6a38b
Dtwosam <42391449+Dtwosam@users.noreply.github.com>
2026-09-04 17:29:00 +0100

    feat: export first FL9 champion builder

--- python/src/shreks_brain/fast_champion/__init__.py
+++ python/src/shreks_brain/fast_champion/__init__.py
@@ -1,3 +1,8 @@
+from .first_builder import (
+    FAST_FIRST_CHAMPION_BUILDER_VERSION,
+    FastFirstChampionBuildResult,
+    build_fast_first_champion,
+)
 from .builder import build_fast_forecast_champion
 from .codec import read_fast_forecast_champion, write_fast_forecast_champion
 from .models import (
@@ -9,6 +14,9 @@ from .models import (
 )
 
 __all__ = (
+    "FAST_FIRST_CHAMPION_BUILDER_VERSION",
+    "FastFirstChampionBuildResult",
+    "build_fast_first_champion",
     "FAST_FORECAST_CHAMPION_SCHEMA_NAME",
     "FAST_FORECAST_CHAMPION_SCHEMA_VERSION",
     "FastForecastChampionSelection",
