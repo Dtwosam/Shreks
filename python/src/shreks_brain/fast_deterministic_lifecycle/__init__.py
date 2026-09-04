5e138b044d4d0572e31c2b6bb193a49c26ee15ac
Dtwosam <42391449+Dtwosam@users.noreply.github.com>
2026-09-04 03:29:20 +0100

    feat: export deterministic lifecycle Python API

---
+++ python/src/shreks_brain/fast_deterministic_lifecycle/__init__.py
@@ -0,0 +1,21 @@
+from .codec import (
+    decode_fast_deterministic_lifecycle_results,
+    fast_deterministic_lifecycle_to_paper_assessment,
+)
+from .models import (
+    FAST_DETERMINISTIC_LIFECYCLE_RESULTS_SCHEMA_NAME,
+    FAST_DETERMINISTIC_LIFECYCLE_RESULTS_SCHEMA_VERSION,
+    FastDeterministicLifecycleDecision,
+    FastDeterministicLifecyclePolicy,
+    FastDeterministicLifecycleResults,
+)
+
+__all__ = [
+    "FAST_DETERMINISTIC_LIFECYCLE_RESULTS_SCHEMA_NAME",
+    "FAST_DETERMINISTIC_LIFECYCLE_RESULTS_SCHEMA_VERSION",
+    "FastDeterministicLifecycleDecision",
+    "FastDeterministicLifecyclePolicy",
+    "FastDeterministicLifecycleResults",
+    "decode_fast_deterministic_lifecycle_results",
+    "fast_deterministic_lifecycle_to_paper_assessment",
+]
